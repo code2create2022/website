5f5cacdf9e08a1a2fb22e6108e9d6a7a2e9e4b10
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2022-07-08 15:35:11 +0530

    Chaned even from keypress to blur

--- script.js
+++ script.js
@@ -3,16 +3,14 @@
 const inputValue = document.querySelector('.answer');
 const dataValidation = document.querySelector('.verify');
 
-document.addEventListener('keypress', function(e)    {
-    if(e.code === "Enter")  {
-        if(inputValue.value == 4)   {
-            dataValidation.innerHTML = "Wrong";
-            dataValidation.style.color = "red";
-        }
-        else    {
-            dataValidation.innerHTML = "Correct!";
-            dataValidation.style.color = "greenyellow";
-        }
+inputValue.addEventListener('blur', function(e)    {
+    if(inputValue.value == 4)   {
+        dataValidation.innerHTML = "Wrong ❌";
+        dataValidation.style.color = "red";
+    }
+    else    {
+        dataValidation.innerHTML = "Correct! ✔️";
+        dataValidation.style.color = "greenyellow";
     }
     }
 )
